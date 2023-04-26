968d681a2c0ce53ad53f0487bbfc0fdcb90601b3
Huynh Phuc Duy <hpduy00vvk@gmail.com>
2023-04-26 13:13:53 +0700

    'search'

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -2,13 +2,14 @@ import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames/bind';
 import styles from './AccountItem.module.scss';
+import Image from '~/components/Image';
 
 const cx = classNames.bind(styles);
 
 function AccountItem() {
     return (
         <div className={cx('wrapper')}>
-            <img
+            <Image
                 className={cx('avatar')}
                 src="https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/8dfd5e868841ab9d807ab0561c2d6097~c5_100x100.jpeg?x-expires=1682398800&x-signature=bF%2BES8NTouDA8X3KC8T41P6Y3JI%3D"
                 alt="Avatar"
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,37 +1,25 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useEffect, useState } from "react";
+import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-  faCircleQuestion,
-  faCircleXmark,
-  faCloudUpload,
-  faCoins,
-  faEarthAsia,
-  faEllipsisVertical,
-  faGear,
-  faKeyboard,
-  faMagnifyingGlass,
-  faSignOut,
-  faSpinner,
-  faUser,
-} from "@fortawesome/free-solid-svg-icons";
-import Tippy from "@tippyjs/react";
-import classNames from "classnames/bind";
+    faCircleQuestion,
+    faCoins,
+    faEarthAsia,
+    faEllipsisVertical,
+    faGear,
+    faKeyboard,
+    faSignOut,
+    faUser,
+} from '@fortawesome/free-solid-svg-icons';
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css';
 
-import Menu from "~/components/Popper/Menu";
-import HeadlessTippy from "@tippyjs/react/headless";
-import "tippy.js/dist/tippy.css";
-import Button from "~/components/Button";
-import { Wrapper as PopperWrapper } from "~/components/Popper";
-import styles from "./Header.module.scss";
-import images from "~/assets/images";
-import {
-  InboxIcon,
-  MessageIcon,
-  SearchIcon,
-  UploadIcon,
-} from "~/components/Icons";
-import Image from "~/components/Image";
-import AccountItem from "~/components/AccountItem";
+import Button from '~/components/Button';
+import styles from './Header.module.scss';
+import images from '~/assets/images';
+import Menu from '~/components/Popper/Menu';
+import { InboxIcon, MessageIcon, UploadIcon } from '~/components/Icons';
+import Image from '~/components/Image';
+import Search from '~/components/Search';
 
 const cx = classNames.bind(styles);
 
@@ -67,12 +55,7 @@ const MENU_ITEMS = [
 ];
 
 function Header() {
-  const [searchResult, setSearchResult] = useState([]);
   const currentUser = true;
-
-  useEffect(() => {
-    setTimeout(() => {});
-  });
 
   // Handle logic
   const handleMenuChange = (menuItem) => {
@@ -115,33 +98,7 @@ function Header() {
         <div className={cx("logo")}>
           <img src={images.logo} alt="Logo-Tiktok" />
         </div>
-        <HeadlessTippy
-          interactive="true"
-          visible={searchResult.length > 0}
-          render={(attrs) => (
-            <div className={cx("search-result")} tabIndex="-1" {...attrs}>
-              <PopperWrapper>
-                <h4 className={cx("search-item")}>Accounts</h4>
-                <AccountItem />
-              </PopperWrapper>
-            </div>
-          )}
-        >
-          <div className={cx("search")}>
-            <input
-              placeholder="Search accounts and videos"
-              spellCheck="false"
-            />
-            <button className={cx("clear-btn")}>
-              <FontAwesomeIcon icon={faCircleXmark} />
-            </button>
-            <FontAwesomeIcon className={cx("loading")} icon={faSpinner} />
-
-            <button className={cx("search-btn")}>
-              <SearchIcon />
-            </button>
-          </div>
-        </HeadlessTippy>
+        <Search />
 
         <div className={cx("action")}>
           {currentUser ? (
