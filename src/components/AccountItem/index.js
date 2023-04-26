468a77e3ffaf99e1d157f340f2ffae19c59789f5
Huynh Phuc Duy <hpduy00vvk@gmail.com>
2023-04-26 13:24:14 +0700

    'search_info'

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -1,28 +1,27 @@
-import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import classNames from 'classnames/bind';
-import styles from './AccountItem.module.scss';
-import Image from '~/components/Image';
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import classNames from "classnames/bind";
+import styles from "./AccountItem.module.scss";
+import Image from "~/components/Image";
+import { Link } from "react-router-dom";
 
 const cx = classNames.bind(styles);
 
-function AccountItem() {
-    return (
-        <div className={cx('wrapper')}>
-            <Image
-                className={cx('avatar')}
-                src="https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/8dfd5e868841ab9d807ab0561c2d6097~c5_100x100.jpeg?x-expires=1682398800&x-signature=bF%2BES8NTouDA8X3KC8T41P6Y3JI%3D"
-                alt="Avatar"
-            />
-            <div className={cx('info')}>
-                <h4 className={cx('name')}>
-                    <span>Nguyen Van A</span>
-                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
-                </h4>
-                <span className={cx('username')}>nguyenvana</span>
-            </div>
-        </div>
-    );
+function AccountItem({ data }) {
+  return (
+    <Link to={`/@${data.nickname}`} className={cx("wrapper")}>
+      <Image className={cx("avatar")} src={data.avatar} alt={data.full_name} />
+      <div className={cx("info")}>
+        <h4 className={cx("name")}>
+          <span>{data.full_name}</span>
+          {data.tick && (
+            <FontAwesomeIcon className={cx("check")} icon={faCheckCircle} />
+          )}
+        </h4>
+        <span className={cx("username")}>{data.nickname}</span>
+      </div>
+    </Link>
+  );
 }
 
 export default AccountItem;
